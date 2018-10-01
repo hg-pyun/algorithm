9e494e33451f24796cf3af38b511a6867d8f1ec7
haegul <phg2491@gmail.com>
2018-10-01 22:38:53 +0900

    update solutions

---
+++ dovelet/CtoF.py
@@ -0,0 +1,6 @@
+# http://59.23.150.58/30stair/CtoF/CtoF.php?pname=CtoF
+
+c = int(input())
+
+f = (9 / 5 * c) + 32
+print('{:.1f}'.format(f))
---
+++ dovelet/q_r.py
@@ -0,0 +1,5 @@
+# http://59.23.150.58/30stair/q_r/q_r.php?pname=q_r
+
+a, b = map(int, input().split())
+
+print(int(a/b), a%b)
