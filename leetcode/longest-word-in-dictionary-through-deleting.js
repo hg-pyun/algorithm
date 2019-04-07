c82d7c545cd2f50f3378b3bed88ac20715472fd6
Haegul Pyun <phg2491@gmail.com>
2019-04-08 00:00:57 +0900

    Create longest-word-in-dictionary-through-deleting.js

---
+++ leetcode/longest-word-in-dictionary-through-deleting.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string[]} d
+ * @return {string}
+ */
+var findLongestWord = function (s, d) {
+
+    let resultList = [];
+
+    d.forEach((word) => {
+        let cursor = 0;
+        for (let i = 0; i < s.length; i++) {
+            const char = s[i];
+
+            if (char === word[cursor]) {
+                cursor++;
+            }
+        }
+
+        if (cursor === word.length) {
+            resultList.push(word);
+        }
+    });
+
+    resultList.sort();
+    return getMaxLengthWord(resultList);
+};
+
+function getMaxLengthWord(list) {
+    let maxLengthWord = '';
+
+    list.forEach((word) => {
+        if(word.length > maxLengthWord.length) {
+            maxLengthWord = word;
+        }
+    });
+
+    return maxLengthWord;
+}
+longest-word-in-dictionary-through-deleting
