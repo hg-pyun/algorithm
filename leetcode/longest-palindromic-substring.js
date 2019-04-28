4772eced0f3f7c9874d22f17e11df48f689707a3
Haegul Pyun <phg2491@gmail.com>
2019-04-28 20:13:20 +0900

    Create longest-palindromic-substring.js

---
+++ leetcode/longest-palindromic-substring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */https://github.com/hg-pyun/algorithm
+var longestPalindrome = function (s) {
+
+    let result = '';
+
+    if(s.length === 1)
+        return s;
+
+    for (let l = 0; l < s.length; l++) {
+        for(let r = s.length - 1; r > 0; r--) {
+            if(s[l] === s[r]) {
+                let subStr = s.substring(l, r+1);
+                if(checkPalindromic(subStr)) {
+                    result = subStr.length > result.length ? subStr : result;
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
+};
+
+function checkPalindromic(s) {
+    let leftCursor = 0;
+    let rightCursor = s.length - 1;
+    let isPalindromic = true;
+
+    while(leftCursor < rightCursor) {
+        if(s[leftCursor++] !== s[rightCursor--]) {
+            isPalindromic = false;
+            break;
+        }
+    }
+
+    return isPalindromic;
+}
