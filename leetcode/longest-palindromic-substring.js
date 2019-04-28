6b6bb80acf09dcd302ff346ccef1f4a0280877d0
Haegul Pyun <phg2491@gmail.com>
2019-04-28 20:14:18 +0900

    Change indent

--- leetcode/longest-palindromic-substring.js
+++ leetcode/longest-palindromic-substring.js
@@ -6,14 +6,14 @@ var longestPalindrome = function (s) {
 
     let result = '';
 
-    if(s.length === 1)
+    if (s.length === 1)
         return s;
 
     for (let l = 0; l < s.length; l++) {
-        for(let r = s.length - 1; r > 0; r--) {
-            if(s[l] === s[r]) {
-                let subStr = s.substring(l, r+1);
-                if(checkPalindromic(subStr)) {
+        for (let r = s.length - 1; r > 0; r--) {
+            if (s[l] === s[r]) {
+                let subStr = s.substring(l, r + 1);
+                if (checkPalindromic(subStr)) {
                     result = subStr.length > result.length ? subStr : result;
                     break;
                 }
@@ -29,8 +29,8 @@ function checkPalindromic(s) {
     let rightCursor = s.length - 1;
     let isPalindromic = true;
 
-    while(leftCursor < rightCursor) {
-        if(s[leftCursor++] !== s[rightCursor--]) {
+    while (leftCursor < rightCursor) {
+        if (s[leftCursor++] !== s[rightCursor--]) {
             isPalindromic = false;
             break;
         }
