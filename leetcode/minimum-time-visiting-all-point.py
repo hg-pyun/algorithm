6757149a345a26bacda7f2b4c76f70f801d24678
Haegul Pyun <phg2491@gmail.com>
2020-09-14 21:58:37 +0900

    Create minimum-time-visiting-all-point.py

---
+++ leetcode/minimum-time-visiting-all-point.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distance(self, p1, p2):
+        diff_x = abs(p1[0] - p2[0])
+        diff_y = abs(p1[1] - p2[1])
+        
+        if diff_x == diff_y:
+            return diff_x
+        else:
+            return max(diff_x, diff_y)
+        
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        ans = 0
+        
+        for i in range(len(points) - 1):
+            ans += self.distance(points[i], points[i+1])
+        
+        return ans
