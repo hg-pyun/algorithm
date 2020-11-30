03fea82f4c720dddd1652953a55ea3aa7b5fd3d8
Haegul Pyun <phg2491@gmail.com>
2020-11-30 22:45:38 +0900

    Update unique-number-of-occurrences.py

--- leetcode/unique-number-of-occurrences.py
+++ leetcode/unique-number-of-occurrences.py
@@ -1,6 +1,6 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        counter = [0 for i in range(1001)]
+        counter = [0 for i in range(2001)]
         for n in arr:
             counter[n] += 1
         m = {}
