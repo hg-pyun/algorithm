8b6b34d2d0e6a5cddd8de72a46b2b192337bd88c
Haegul Pyun <phg2491@gmail.com>
2020-11-30 22:18:25 +0900

    Create unique-number-of-occurrences.py

---
+++ leetcode/unique-number-of-occurrences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counter = [0 for i in range(1001)]
+        for n in arr:
+            counter[n] += 1
+        m = {}
+        for n in counter:
+            if n == 0:
+                continue
+            if n in m:
+                return False
+            else:
+                m[n] = 1
+        return True
