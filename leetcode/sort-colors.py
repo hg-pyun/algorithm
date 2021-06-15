f70a1248574164df9c7e3ddddc95aac09422d20d
Haegul Pyun <phg2491@gmail.com>
2021-06-15 21:52:07 +0900

    Create sort-colors.py

---
+++ leetcode/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            for j in range(0, len(nums) - i - 1):
+                if nums[j] > nums[j+1]:
+                    temp = nums[j]
+                    nums[j] = nums[j+1]
+                    nums[j+1] = temp
