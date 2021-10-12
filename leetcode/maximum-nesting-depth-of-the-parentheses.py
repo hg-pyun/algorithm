8e37205ba88714b2fb7bb1919c7c1bad31e24221
Haegul Pyun <phg2491@gmail.com>
2021-10-12 22:32:32 +0900

    Update maximum-nesting-depth-of-the-parentheses.py

--- leetcode/maximum-nesting-depth-of-the-parentheses.py
+++ leetcode/maximum-nesting-depth-of-the-parentheses.py
@@ -1,15 +1,13 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack = []
         
         ans = 0
         dep = 0
+        
         for char in s:
             if char == '(':
-                stack.append('(')
                 dep += 1
             elif char == ')':
-                stack.pop()
                 ans = max(dep, ans)
                 dep -= 1
         
