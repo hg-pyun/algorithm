960dd1de20ce30ec1d9e1e157b0860f62d7d39c0
Haegul Pyun <phg2491@gmail.com>
2022-04-05 21:53:41 +0900

    Create largest-triangle-area.py

---
+++ leetcode/largest-triangle-area.py
@@ -0,0 +1,23 @@
+class Solution:
+    def get_triangle_area(self, p1, p2, p3):
+        first = (p1[0] * p2[1]) + (p2[0] * p3[1]) + (p3[0] * p1[1])
+        second = (p1[1] * p2[0]) + (p2[1] * p3[0]) + (p3[1] * p1[0])
+        
+        re = (first - second) * 0.5
+        if re >= 0:
+            return re
+        else:
+            return re * - 1
+        
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        
+        length = len(points)
+        
+        ans = 0
+        
+        for i in range(length):
+            for j in range(i + 1):
+                for k in range(j + 1):
+                    ans = max(ans, self.get_triangle_area(points[i], points[j], points[k]))
+        
+        return ans
