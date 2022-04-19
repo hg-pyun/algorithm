82c317cc96b7d53c10c1b7b6bf9fe2d6086665c5
Haegul Pyun <phg2491@gmail.com>
2022-04-19 21:43:08 +0900

    Create find-pivot-index.py

---
+++ leetcode/find-pivot-index.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        
+        for num in nums:
+            right += num;
+            
+        ans = -1
+        
+        for i in range(len(nums)):
+            right -= nums[i]
+            if left == right:
+                ans = i
+                break
+            
+            left += nums[i]
+        
+        return ans
